3140db689606dbb936a09b8824397439b69a5341
Wiryono Lauw <wiryonolau@gmail.com>
2019-03-26 20:26:31 +0700

    Fix check condition, add on off prioirty, add logic gate

--- rpioalert/__main__.py
+++ rpioalert/__main__.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-import asyncio
 import argparse
+import asyncio
 import json
-import sys
 import logging
 import signal
-from gpiozero import LED
+import sys
 from concurrent.futures import ThreadPoolExecutor
+
+from gpiozero import LED
+
 from .temper import Temper
+
 
 class Status:
     def __init__(self, temperature=0, humidity=0):
@@ -31,7 +34,8 @@ class Status:
         self._humidity = hum
 
     def dict(self):
-        return {"temperature" : self._temperature, "humidity" : self._humidity}
+        return {"temperature": self._temperature, "humidity": self._humidity}
+
 
 def get_status(temper):
     logger = logging.getLogger("rpioalert.get_status")
@@ -59,51 +63,123 @@ def get_status(temper):
 
     return status
 
-def compare(condition, temperature, humidity):
+
+def compare(comparison, value_to_compare, current_value):
     try:
-        value_type, comparison, cvalue = condition.split(":")
-
-        if value_type == ["t", "temp", "temperature"]:
-            value = temperature
-        elif value_type in ["h", "hum", "humidity"]:
-            value = humidity
-
         if comparison == "eq":
-            return float(value) == float(cvalue)
+            return float(current_value) == float(value_to_compare)
         elif comparison == "gt":
-            return float(value) > float(cvalue)
+            return float(current_value) > float(value_to_compare)
         elif comparison == "gte":
-            return float(value) >= float(cvalue)
+            return float(current_value) >= float(value_to_compare)
         elif comparison == "lt":
-            return float(value) < float(cvalue)
+            return float(current_value) < float(value_to_compare)
         elif comparison == "lte":
-            return float(value) <= float(cvalue)
+            return float(current_value) <= float(value_to_compare)
         else:
             return False
     except:
         return False
 
-def toggle_led(leds, condition, avg_temp, avg_humid, turn_on):
-    logger = logging.getLogger("rpioalert.toggle_led")
-    reach = True
-    for c in condition:
-        reach = (reach and compare(c, avg_temp, avg_humid))
 
+def logic_gate(logic, value_left, value_right):
+    if logic == "or":
+        result = (value_left or value_right)
+    elif logic == "xor":
+        result = (bool(value_left) != bool(value_right))
+    elif logic == "nand":
+        result = not (value_left and value_right)
+    elif logic == "nor":
+        result = not (value_left or value_right)
+    elif logic == "xnor":
+        result = not (bool(value_left) != bool(value_right))
+    else:
+        # Default AND
+        result = (value_left and value_right)
+
+    return result
+
+
+def format_condition(condition, as_str=False):
+    condition_configs = []
+    for c in condition:
+        condition_config = c.split(":")
+        if len(condition_config) == 3:
+            # Added defult logic gate
+            condition_config.append("and")
+            condition_configs.append(condition_config)
+        elif len(condition_config) == 4:
+            condition_configs.append(condition_config)
+        else:
+            # Remove invalid condition format
+            continue
+
+    if as_str is False:
+        return condition_configs
+
+    condition_str = []
+    for i, c in enumerate(condition_configs):
+        value_type, comparison, value_to_compare, logic = c
+        if i > 0:
+            condition_str.append(logic)
+        condition_str.append("{}:{}:{}".format(
+            value_type, comparison, value_to_compare))
+
+    return " ".join(condition_str)
+
+
+def toggle_led(leds, condition, avg_temp, avg_humid, turn_on):
+    """
+    Toggle led if condition is Reach
+    return bool(reach)
+    """
+    logger = logging.getLogger("rpioalert.toggle_led")
+    reach = None
+
+    condition_configs = format_condition(condition)
+    condition_str = format_condition(condition, True)
+
+    for c in condition_configs:
+        value_type, comparison, value_to_compare, logic = c
+
+        if value_type in ["t", "temp", "temperature"]:
+            current_value = avg_temp
+        elif value_type in ["h", "hum", "humidity"]:
+            current_value = avg_humid
+        else:
+            continue
+
+        compare_result = compare(comparison, value_to_compare, current_value)
+        if reach is None:
+            reach = compare_result
+            continue
+
+        reach = logic_gate(logic, reach, compare_result)
+
+    logger.debug("{} : {}, T:{}, H:{}, Reach:{}".format(
+        "ON" if turn_on else "OFF", condition_str, avg_temp, avg_humid, reach))
+
+    # Not reach
     if reach is False:
         return False
 
-    logger.debug("{}, T:{}, H:{}, Reach:{}".format(",".join(condition), avg_temp, avg_humid, reach))
-
     for led in leds:
         if turn_on is True and led.is_lit is False:
-            logger.debug("{}, T:{}, H:{}, LED:{}, OFF->ON".format(",".join(condition), avg_temp, avg_humid, led.pin.number))
+            logger.debug("{}, T:{}, H:{}, LED:{}, OFF->ON".format(
+                condition_str, avg_temp, avg_humid, led.pin.number))
             led.on()
         elif turn_on is False and led.is_lit is True:
-            logger.debug("{}, T:{}, H:{}, LED:{}, ON->OFF".format(",".join(condition), avg_temp, avg_humid, led.pin.number))
-            led.off() 
+            logger.debug("{}, T:{}, H:{}, LED:{}, ON->OFF".format(
+                condition_str, avg_temp, avg_humid, led.pin.number))
+            led.off()
 
-    current_state = ["LED:{} {}".format(l.pin.number, "ON" if l.is_lit else "OFF") for l in leds]
+    current_state = ["LED:{} {}".format(
+        l.pin.number, "ON" if l.is_lit else "OFF") for l in leds]
     logger.debug("Current state {}".format(", ".join(current_state)))
+
+    # Reach
+    return True
+
 
 async def rpc_server(leds, stats, listen="0.0.0.0", port=15555, off_condition=[], on_condition=[], lock=None, executor=None, loop=None):
     executor = executor or ThreadPoolExecutor(max_workers=1)
@@ -121,8 +197,10 @@ async def rpc_server(leds, stats, listen="0.0.0.0", port=15555, off_condition=[]
         try:
             if request["method"] == "get_status":
                 async with lock:
-                    led_state = [{"pin" : l.pin.number, "state": l.is_lit} for l in leds]
-                    current_state = {"status" : stats.dict(), "condition": {"off": off_condition, "on": on_condition }, "led": led_state}
+                    led_state = [{"pin": l.pin.number, "state": l.is_lit}
+                                 for l in leds]
+                    current_state = {"status": stats.dict(), "condition": {
+                        "off": off_condition, "on": on_condition}, "led": led_state}
                     response = json.dumps(current_state)
 
             logger.debug("Response : {}".format(response))
@@ -141,7 +219,8 @@ async def rpc_server(leds, stats, listen="0.0.0.0", port=15555, off_condition=[]
     except:
         logger.info(sys.exc_info())
 
-async def rpio_alert(leds, stats, off_condition=[], on_condition=[], lock=None, executor=None, both=False, loop=None):
+
+async def rpio_alert(leds, stats, off_condition=[], on_condition=[], off_first=False, lock=None, executor=None, both=False, loop=None):
     executor = executor or ThreadPoolExecutor(max_workers=1)
     loop = loop or asyncio.get_event_loop()
     logger = logging.getLogger("rpioalert.rpio_alert")
@@ -163,7 +242,6 @@ async def rpio_alert(leds, stats, off_condition=[], on_condition=[], lock=None, 
                 if "internal_temperature" in s:
                     temps.append(s["internal_temperature"] or 0)
 
-
                 if "internal_humidity" in s:
                     humis.append(s["internal_humidity"] or 0)
 
@@ -177,8 +255,16 @@ async def rpio_alert(leds, stats, off_condition=[], on_condition=[], lock=None, 
                 stats.temperature = avg_temp
                 stats.humidity = avg_humid
 
-                toggle_led(leds, off_condition, avg_temp, avg_humid, turn_on=False)
-                toggle_led(leds, on_condition, avg_temp, avg_humid, turn_on=True)
+                if off_first:
+                    turn_on = [False, True]
+                else:
+                    turn_on = [True, False]
+
+                reach = toggle_led(leds, off_condition, avg_temp,
+                                   avg_humid, turn_on=turn_on[0])
+                if not reach:
+                    toggle_led(leds, on_condition, avg_temp,
+                               avg_humid, turn_on=turn_on[1])
         except asyncio.CancelledError:
             stop = True
         except KeyboardInterrupt:
@@ -188,20 +274,32 @@ async def rpio_alert(leds, stats, off_condition=[], on_condition=[], lock=None, 
 
         await asyncio.sleep(1)
 
+
 async def shutdown(task):
     task.cancel()
     await task
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pin", help="GPIO Pin", type=int, action='append')
-    parser.add_argument("--off", help="Pin Off condition, format: <temp|hum>:<eq|lt|lte|gt|gte>:<value>", action="append", default=[])
-    parser.add_argument("--on", help="Pin On condition, format: <temp|hum>:<eq|lt|lte|gt|gte>:<value>", action="append", default=[])
-    parser.add_argument("-rpc", help="Start rpc server", action="store_true", default=False)
-    parser.add_argument("--rpc_listen", help="Listen address, default all 0.0.0.0", type=str, default="0.0.0.0")
-    parser.add_argument("--rpc_port", help="Listen port, default 15555", type=int, default=15555)
-    parser.add_argument("-stop", help="Cleanup on stop service", action="store_true", default=False)
-    parser.add_argument("-v", "--verbose", help="Log verbosity", action="store_true", default=False)
+    parser.add_argument("-rpc", help="Start rpc server",
+                        action="store_true", default=False)
+    parser.add_argument("-v", "--verbose", help="Log verbosity",
+                        action="store_true", default=False)
+    parser.add_argument("-stop", help="Cleanup on stop service",
+                        action="store_true", default=False)
+    parser.add_argument(
+        "-off_first", help="Check OFF condition first, then ON condition", action="store_true", default=False)
+    parser.add_argument("--pin", help="GPIO Pin", type=int,
+                        action='append', default=[])
+    parser.add_argument(
+        "--off", help="Pin Off condition, format: <temp|hum>:<eq|lt|lte|gt|gte>:<value>:[or|and|xor|nand|nor|xnor]", action="append", default=[])
+    parser.add_argument(
+        "--on", help="Pin On condition, format: <temp|hum>:<eq|lt|lte|gt|gte>:<value>:[or|and|xor|nand|nor|xnor]", action="append", default=[])
+    parser.add_argument(
+        "--rpc_listen", help="Listen address, default all 0.0.0.0", type=str, default="0.0.0.0")
+    parser.add_argument(
+        "--rpc_port", help="Listen port, default 15555", type=int, default=15555)
 
     args = parser.parse_args()
 
@@ -210,11 +308,6 @@ def main():
         format="%(asctime)s %(levelname)-8s %(name)-30s %(message)s"
     )
     logger = logging.getLogger("rpioalert.main")
-
-    # Allow empy pin, only status
-    # if args.pin is None:
-    #     logger.info("Please state GPIO pin")
-    #     sys.exit()
 
     if args.stop is True:
         logger.info("Reset LED")
@@ -229,7 +322,6 @@ def main():
     except:
         logger.info("Unable to connect to GPIO Pin {}".format(args.pin))
         logger.debug(sys.exc_info())
-        sys.exit()
 
     loop = asyncio.get_event_loop()
     executor = ThreadPoolExecutor(max_workers=1)
@@ -239,28 +331,29 @@ def main():
 
     tasks = [
         asyncio.ensure_future(rpio_alert(**{
-            "leds" : leds,
-            "off_condition" : args.off,
-            "on_condition" : args.on,
-            "stats" : stats,
-            "lock" : lock,
-            "executor" : executor,
-            "loop" : loop
+            "leds": leds,
+            "off_condition": args.off,
+            "on_condition": args.on,
+            "off_first": args.off_first,
+            "stats": stats,
+            "lock": lock,
+            "executor": executor,
+            "loop": loop
         }))
     ]
 
     if args.rpc:
         tasks.append(
             asyncio.ensure_future(rpc_server(**{
-                "leds" : leds,
-                "listen" : args.rpc_listen,
-                "port" : args.rpc_port,
-                "off_condition" : args.off,
-                "on_condition" : args.on,
-                "stats" : stats,
-                "lock" : lock,
-                "executor" : executor,
-                "loop" : loop
+                "leds": leds,
+                "listen": args.rpc_listen,
+                "port": args.rpc_port,
+                "off_condition": args.off,
+                "on_condition": args.on,
+                "stats": stats,
+                "lock": lock,
+                "executor": executor,
+                "loop": loop
             }))
         )
 
@@ -283,6 +376,7 @@ def main():
         if not led.closed:
             led.off()
             led.close()
+
 
 if __name__ == "__main__":
     main()
